e25ddc280b88e5fa48e2fe52808c089ac9b6c41e
Salvucci Facundo <salvucci1288@gmail.com>
2025-12-25 17:56:17 -0300

    about refactor datos

--- src/app/features/about/about.component.ts
+++ src/app/features/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject, computed } from '@angular/core';
 import { PortfolioStateService } from '../../core/services/portfolio-state.service';
 import { CommonModule } from '@angular/common';
 
@@ -8,88 +8,87 @@ import { CommonModule } from '@angular/common';
   template: `
     <div class="code-container">
       @if (state.profile(); as p) {
-      <div class="line"><span class="ln">1</span><span class="comment">/**</span></div>
       <div class="line">
-        <span class="ln">2</span><span class="comment"> * @system identity_module</span>
+        <span class="ln">1</span><span class="keyword">export interface</span>&nbsp;<span
+          class="type"
+          >SystemOperator</span
+        >
+        &#123;
       </div>
       <div class="line">
-        <span class="ln">3</span><span class="comment"> * @operator {{ p.name }}</span>
-      </div>
-      <div class="line"><span class="ln">4</span><span class="comment"> */</span></div>
-      <div class="line"><span class="ln">5</span></div>
-      <div class="line">
-        <span class="ln">6</span><span class="keyword">export interface</span>
-        <span class="type">SystemOperator</span> &#123;
-      </div>
-      <div class="line">
-        <span class="ln">7</span>&nbsp;&nbsp;<span class="prop">fullName</span>:
+        <span class="ln">2</span>&nbsp;&nbsp;<span class="prop">fullName</span>:
         <span class="type">string</span>;
       </div>
       <div class="line">
-        <span class="ln">8</span>&nbsp;&nbsp;<span class="prop">role</span>:
+        <span class="ln">3</span>&nbsp;&nbsp;<span class="prop">role</span>:
         <span class="type">string</span>;
       </div>
       <div class="line">
-        <span class="ln">9</span>&nbsp;&nbsp;<span class="prop">location</span>:
-        <span class="type">string</span>;
-      </div>
-      <div class="line">
-        <span class="ln">10</span>&nbsp;&nbsp;<span class="prop">status</span>:
+        <span class="ln">4</span>&nbsp;&nbsp;<span class="prop">status</span>:
         <span class="string">'OPEN_TO_WORK'</span> | <span class="string">'BUSY'</span>;
       </div>
       <div class="line">
-        <span class="ln">11</span>&nbsp;&nbsp;<span class="prop">stack</span>:
+        <span class="ln">5</span>&nbsp;&nbsp;<span class="prop">education</span>: &#123;
+        <span class="prop">degree</span>: <span class="type">string</span>;
+        <span class="prop">uni</span>: <span class="type">string</span> &#125;;
+      </div>
+      <div class="line">
+        <span class="ln">6</span>&nbsp;&nbsp;<span class="prop">languages</span>:
         <span class="type">string</span>[];
       </div>
       <div class="line">
-        <span class="ln">12</span>&nbsp;&nbsp;<span class="prop">availability</span>:
+        <span class="ln">7</span>&nbsp;&nbsp;<span class="prop">availability</span>:
         <span class="type">string</span>;
       </div>
-      <div class="line"><span class="ln">13</span>&#125;</div>
-      <div class="line"><span class="ln">14</span></div>
+      <div class="line"><span class="ln">8</span>&#125;</div>
+      <div class="line"><span class="ln">9</span></div>
       <div class="line">
-        <span class="ln">15</span><span class="keyword">const</span>
-        <span class="const">OPERATOR_DATA</span>: <span class="type">SystemOperator</span> = &#123;
+        <span class="ln">10</span><span class="keyword">const</span>&nbsp;<span class="const"
+          >OPERATOR_DATA</span
+        >: <span class="type">SystemOperator</span> = &#123;
       </div>
       <div class="line">
-        <span class="ln">16</span>&nbsp;&nbsp;<span class="prop">fullName</span>:
+        <span class="ln">11</span>&nbsp;&nbsp;<span class="prop">fullName</span>:
         <span class="string">'{{ p.name }}'</span>,
       </div>
       <div class="line">
-        <span class="ln">17</span>&nbsp;&nbsp;<span class="prop">role</span>:
+        <span class="ln">12</span>&nbsp;&nbsp;<span class="prop">role</span>:
         <span class="string">'{{ p.role }}'</span>,
       </div>
       <div class="line">
-        <span class="ln">18</span>&nbsp;&nbsp;<span class="prop">location</span>:
-        <span class="string">'{{ p.location }} ({{ p.timezone }})'</span>,
-      </div>
-      <div class="line">
-        <span class="ln">19</span>&nbsp;&nbsp;<span class="prop">status</span>:
+        <span class="ln">13</span>&nbsp;&nbsp;<span class="prop">status</span>:
         <span class="string">'{{ p.status === 'Open to Work' ? 'OPEN_TO_WORK' : 'BUSY' }}'</span>,
       </div>
       <div class="line">
-        <span class="ln">20</span>&nbsp;&nbsp;<span class="prop">stack</span>: [<span class="string"
-          >'Angular'</span
-        >, <span class="string">'TypeScript'</span>, <span class="string">'Firebase'</span>,
-        <span class="string">'RxJS'</span>],
+        <span class="ln">14</span>&nbsp;&nbsp;<span class="prop">education</span>: &#123;
       </div>
       <div class="line">
-        <span class="ln">21</span>&nbsp;&nbsp;<span class="prop">availability</span>:
+        <span class="ln">15</span>&nbsp;&nbsp;&nbsp;&nbsp;<span class="prop">degree</span>:
+        <span class="string">'{{ p.education?.degree }}'</span>,
+      </div>
+      <div class="line">
+        <span class="ln">16</span>&nbsp;&nbsp;&nbsp;&nbsp;<span class="prop">uni</span>:
+        <span class="string">'{{ p.education?.university }}'</span>
+      </div>
+      <div class="line"><span class="ln">17</span>&nbsp;&nbsp;&#125;,</div>
+      <div class="line">
+        <span class="ln">18</span>&nbsp;&nbsp;<span class="prop">languages</span>: [ @for (lang of
+        p.languages; track lang.name; let last = $last) {
+        <span class="string">'{{ lang.name }}'</span>{{ last ? '' : ', ' }}
+        } ],
+      </div>
+      <div class="line">
+        <span class="ln">19</span>&nbsp;&nbsp;<span class="prop">availability</span>:
         <span class="string">'{{ p.availability }}'</span>
       </div>
-      <div class="line"><span class="ln">22</span>&#125;;</div>
-      <div class="line"><span class="ln">23</span></div>
+      <div class="line"><span class="ln">20</span>&#125;;</div>
+      <div class="line"><span class="ln">21</span></div>
       <div class="line">
-        <span class="ln">24</span><span class="comment">// Professional Brief</span>
+        <span class="ln">22</span><span class="keyword">const</span>&nbsp;<span class="prop"
+          >bio</span
+        >
+        = <span class="string">&#96;{{ truncatedBio() }}&#96;</span>;
       </div>
-      <div class="line">
-        <span class="ln">25</span><span class="keyword">const</span> <span class="prop">bio</span> =
-        <span class="string">&#96;</span>
-      </div>
-      <div class="line">
-        <span class="ln">26</span><span class="string">&nbsp;&nbsp;{{ p.bio }}</span>
-      </div>
-      <div class="line"><span class="ln">27</span><span class="string">&#96;</span>;</div>
       } @else {
       <div class="line">
         <span class="ln">1</span
@@ -144,4 +143,9 @@ import { CommonModule } from '@angular/common';
 })
 export class AboutComponent {
   protected state = inject(PortfolioStateService);
+
+  truncatedBio = computed(() => {
+    const bio = this.state.profile()?.bio || '';
+    return bio.length > 80 ? bio.substring(0, 77) + '...' : bio;
+  });
 }
